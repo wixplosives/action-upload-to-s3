33334d52cd7938cad633ec4a709e5cc971e62b7f
Alex Shemshurenko <alexshe@wix.com>
2021-03-10 10:43:38 +0200

    feat: virtual project for specific file (#55)
    * ci: upload specific file
    
    * feat: specific file overrides default project
    
    * feat: build new feature

--- src/main.ts
+++ src/main.ts
@@ -10,14 +10,27 @@ async function run(): Promise<void> {
     const sourceFolder: string = core.getInput('sourceFolder')
     const tags: string = core.getInput('tags')
     const publishAsProject: string = core.getInput('publishAsVirtualProject')
+    const specificFile: string = core.getInput('specificFile')
     core.info(`Uploading ${sourceFolder} to ${awsBucket}/${s3Subfolder}`)
     const s3Client = new AWSS3Client(accessKeyId, secretAccessKey)
     await s3Client.uploadFolder(awsBucket, s3Subfolder, sourceFolder, tags)
     if (publishAsProject && publishAsProject !== '') {
-      await s3Client.updateLatest(awsBucket, s3Subfolder, publishAsProject)
-      core.info(
-        `Create static link ${awsBucket}/${publishAsProject} for ${awsBucket}/${s3Subfolder}`
-      )
+      if (specificFile !== '') {
+        await s3Client.updateLatestWithSpecificFile(
+          awsBucket,
+          s3Subfolder,
+          publishAsProject,
+          specificFile
+        )
+        core.info(
+          `Create static link ${awsBucket}/${publishAsProject} for ${awsBucket}/${s3Subfolder}/${specificFile}`
+        )
+      } else {
+        await s3Client.updateLatest(awsBucket, s3Subfolder, publishAsProject)
+        core.info(
+          `Create static link ${awsBucket}/${publishAsProject} for ${awsBucket}/${s3Subfolder}`
+        )
+      }
     }
     core.info('Done')
   } catch (error) {
